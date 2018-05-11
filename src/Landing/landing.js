7f64f91b83a3e2e8961854cfe5c7dffda7fa5ee5
Aleksi Savijoki <aleksi.savijoki@gmail.com>
2018-05-11 12:32:54 +0300

    Fixed landing section

---
+++ src/Landing/landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import arrowDown from '../images/arrow-down.png';
+import './landing.css';
+import ScrollIntoView from 'react-scroll-into-view';
+
+export default class Landing extends React.Component {
+  render() {
+    return (
+      <section id="landing" className="valign-wrapper">
+        <div>
+          <h1 className="display-3 text-white">Portfolio</h1>
+          <h4 className="text-white text-center animated fadeIn">By Aleksi Savijoki</h4>
+        </div>
+        <div className="arrow-container animated fadeInDownBig">
+          <ScrollIntoView selector="#intro">
+            <img src={arrowDown} alt="Next section"/>
+          </ScrollIntoView>
+        </div>
+      </section>
+    );
+  }
+}
